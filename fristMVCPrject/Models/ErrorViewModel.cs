f65f1a145898f84977149b4c8a59e62cc76059f1
HamdyAmr60 <hamdyamr60@gmail.com>
2024-07-29 00:57:35 +0300

    Add project files.

---
+++ fristMVCPrject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fristMVCPrject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
