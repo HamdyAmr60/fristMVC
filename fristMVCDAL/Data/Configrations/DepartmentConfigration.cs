ee4e074492b3a5b4c8796c1804e9d9f8208bfd5e
HamdyAmr60 <hamdyamr60@gmail.com>
2024-07-29 02:54:50 +0300

    add connection string and migrations

---
+++ fristMVCDAL/Data/Configrations/DepartmentConfigration.cs
@@ -0,0 +1,21 @@
+﻿using fristMVCDAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fristMVCDAL.Data.Configrations
+{
+    internal class DepartmentConfigration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+            builder.Property(D => D.Code).HasColumnType("nvarchar").HasMaxLength(50).IsRequired();
+            builder.Property(D => D.Name).HasColumnType("nvarchar").HasMaxLength(50).IsRequired();
+        }
+    }
+}
--- fristMVCPrject/Startup.cs
+++ fristMVCPrject/Startup.cs
@@ -1,12 +1,15 @@
+using fristMVCDAL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace fristMVCPrject
@@ -24,6 +27,11 @@ namespace fristMVCPrject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("default"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
